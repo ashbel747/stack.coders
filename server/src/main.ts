2453bb1b1e6ea1eb3487a487a09ccefcf98fbf24
Ashbel <kashbel747@gmail.com>
2025-10-19 10:10:14 +0300

    Add deployed frontend link to CORS whitelist

--- server/src/main.ts
+++ server/src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',        // for local development
+      'https://stack-coders.vercel.app/',        //for deployed frontend
     ],
   });
 
